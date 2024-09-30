fad834efde3d20f91c50758513d73ba5743ad142
harshita <harshita@logicrays.com>
2024-09-30 16:28:38 +0530

    first

---
+++ react-basics/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <h2>Welcome to LRA</h2>
+    </div>
+  );
+}
+
+export default App;
