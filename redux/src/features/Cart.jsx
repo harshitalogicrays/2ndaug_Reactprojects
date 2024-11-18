7ed852bb79758b935e5424a2fc178d3ddcd349ab
harshita <harshita@logicrays.com>
2024-11-18 13:23:04 +0530

    18nov

--- redux/src/features/Cart.jsx
+++ redux/src/features/Cart.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectCart } from '../redux/cartSlice'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { CALCULATE_TOTAL, DECREASE, EMPTY_CART, INCREASE, REMOVE_FROM_CART, selectCart, selectTotal } from '../redux/cartSlice'
 import { BsTrash } from 'react-icons/bs'
 
 const Cart = () => {
   const cartItems = useSelector(selectCart)
+  const dispatch =useDispatch()
+  const total = useSelector(selectTotal)
+  useEffect(()=>{dispatch(CALCULATE_TOTAL())},[cartItems])
   return (
     <div className='container mt-5 shadow p-3'>
       <h1>Cart Page</h1>
@@ -29,23 +32,24 @@ const Cart = () => {
               <td>${c.price}</td>
               <td>
                 <div className="input-group">
-                  <button type="button" className='btn btn-primary'>-</button>
+                  <button type="button" className='btn btn-primary' onClick={()=>dispatch(DECREASE(c))}>-</button>
                   <input type="text" value={c.qty}  className="form-control" style={{width:'20px',textAlign:'center'}}/>
-                  <button type="button" className='btn btn-primary'>+</button>
+                  <button type="button" className='btn btn-primary' 
+                  onClick={()=>dispatch(INCREASE(c))}>+</button>
                 </div>
               </td>
               <td>${c.price * c.qty}</td>
-              <td><button type="button" className='btn btn-danger'><BsTrash/></button></td>
+              <td><button type="button" className='btn btn-danger' onClick={()=>dispatch(REMOVE_FROM_CART(c.id))}><BsTrash/></button></td>
             </tr>)}
           </tbody>
         </table>
       </div>
   <div className="row">
     <div className="col-9">
-      <button type="button" class="btn btn-danger btn-lg" > Empty Cart </button>
+      <button type="button" class="btn btn-danger btn-lg" onClick={()=>dispatch(EMPTY_CART())}> Empty Cart </button>
     </div>
     <div className="col">
-      <h5>Total:<span class="float-end">$</span></h5><hr/>
+      <h5>Total:<span class="float-end">${total.toFixed(2)}</span></h5><hr/>
       <div class="d-grid gap-2">
         <button  type="button" class="btn btn-warning" >
           Checkcout
--- redux/src/redux/cartSlice.js
+++ redux/src/redux/cartSlice.js
@@ -14,10 +14,28 @@ const cartSlice = createSlice({
             }
             else toast.info(`${action.payload.title} already added to cart`) 
         },
-        INCREASE(state,action){}
-    }
-})
+        INCREASE(state,action){
+            const itemIndex = state.cartItems.findIndex(item=>item.id==action.payload.id)
+            if(  state.cartItems[itemIndex].qty < action.payload.stock){ state.cartItems[itemIndex].qty++   }
+            else toast.info(`only ${action.payload.stock} qty available`)    },
+        DECREASE(state,action){
+            const itemIndex = state.cartItems.findIndex(item=>item.id==action.payload.id)
+            if(  state.cartItems[itemIndex].qty > 1){
+                state.cartItems[itemIndex].qty--  }
+            else  state.cartItems[itemIndex].qty=1
+        },
+        REMOVE_FROM_CART(state,action){
+            const itemIndex = state.cartItems.findIndex(item=>item.id==action.payload)
+            state.cartItems.splice(itemIndex,1)
+
+            // const filters = state.cartItems.filter(item=>item.id != action.payload)
+            // state.cartItems = [...filters]
+        },
+        EMPTY_CART(state,action){ state.cartItems=[];state.total=0},
+        CALCULATE_TOTAL(state,action){
+          state.total = state.cartItems.reduce((prev,curr)=>{return prev+(curr.price*curr.qty)},0)}}})
 // console.log(cartSlice.actions)
-export const {ADD_TO_CART}  =cartSlice.actions
+export const {ADD_TO_CART,INCREASE,DECREASE,REMOVE_FROM_CART,CALCULATE_TOTAL,EMPTY_CART}  =cartSlice.actions
 export default cartSlice
 export const selectCart = (state)=>state.cart.cartItems
+export const selectTotal = (state)=>state.cart.total
